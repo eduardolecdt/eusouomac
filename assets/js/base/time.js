12baaa4f8890d008118f7938bc6bc16a36340e27
Luiz Eduardo <eduardolecdt@gmail.com>
2020-07-01 04:46:53 -0300

    Ajustes Monetizando Online

--- assets/js/base/time.js
+++ assets/js/base/time.js
@@ -1,19 +1,25 @@
-var deadline = new Date("apr 2, 2022 23:59:00").getTime();
+var countDownDate = new Date("Jun 26, 2020 23:59:00").getTime();
 
 var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-var now = new Date().getTime();
-var t = deadline - now;
-var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-var seconds = Math.floor((t % (1000 * 60)) / 1000);
-document.getElementById("hora").innerHTML =hours;
-document.getElementById("minuto").innerHTML = minutes;
-document.getElementById("segundo").innerHTML =seconds;
-if (t < 0) {
-        clearInterval(x);
-        document.getElementById("text").innerHTML = "Desculpe! O carrinho não está mais aberto.";
-        document.getElementById("hora").innerHTML ='0';
-        document.getElementById("minuto").innerHTML ='0' ;
-        document.getElementById("segundo").innerHTML = '0'; }
+      document.getElementById("dia").innerHTML = days;
+      document.getElementById("hora").innerHTML = hours;
+      document.getElementById("minuto").innerHTML = minutes;
+      document.getElementById("segundo").innerHTML = seconds;
+
+   if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("text").innerHTML = "Desculpe! O treinamento está fechado.";
+      document.getElementById("dia").innerHTML ='0';
+      document.getElementById("hora").innerHTML ='0';
+      document.getElementById("minuto").innerHTML ='0' ;
+      document.getElementById("segundo").innerHTML = '0';
+   }
+
 }, 1000);

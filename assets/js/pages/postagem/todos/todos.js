aec9b3dab065f9346dada931205b474037b508aa
Luiz Eduardo <eduardolecdt@gmail.com>
2020-07-28 05:46:06 -0300

    Index criada + /postagem modelo 1 ajustado e URLs refeitas

--- assets/js/pages/postagem/todos/todos.js
+++ assets/js/pages/postagem/todos/todos.js
@@ -37,7 +37,7 @@
     if (!_isAndroid()) return
     if (!_isInAppBrowser()) return
 
-    window.location = 'intent://eusouomac.com/ferramenta/postagem#Intent;scheme=https;package=com.android.chrome;end'
+    window.location = 'intent://eusouomac.com/postagem/todos#Intent;scheme=https;package=com.android.chrome;end'
   }
 
   redirectAndroidInAppBrowsers()

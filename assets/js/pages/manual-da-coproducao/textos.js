6feff55ceaf691735afd485a83360beed93bf042
Luiz Eduardo <eduardolecdt@gmail.com>
2020-07-30 18:04:50 -0300

    Google analytics, facebook, ads

--- assets/js/pages/manual-da-coproducao/textos.js
+++ assets/js/pages/manual-da-coproducao/textos.js
@@ -1,4 +1,4 @@
-document.querySelector('#preco_total').innerText = 'R$0,00'
-document.querySelector('#link_1').href = ''
-document.querySelector('#link_2').href = ''
-document.querySelector('#link_3').href = ''
+document.querySelector('#preco_total').innerText = 'R$9,00'
+document.querySelector('#link_1').href = 'https://pay.hotmart.com/B27887279V?checkoutMode=10&bid=1596138597192'
+document.querySelector('#link_2').href = 'https://pay.hotmart.com/B27887279V?checkoutMode=10&bid=1596138597192'
+document.querySelector('#link_3').href = 'https://pay.hotmart.com/B27887279V?checkoutMode=10&bid=1596138597192'
